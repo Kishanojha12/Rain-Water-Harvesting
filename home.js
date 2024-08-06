89614b9cb33dcd7b733a66a204455df0616fcb87
Kishan Ojha <129095045+Kishanojha12@users.noreply.github.com>
2024-08-06 11:51:45 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,11 @@
+/* script.js */
+
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
